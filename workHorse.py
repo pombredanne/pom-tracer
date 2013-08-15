316fd237ab6c2a7d6eae37b13942f2132593d516
Dan Catalano <dcatalano@thetus.com>
2013-08-15 16:55:28 -0700

    one full iteration of parsing complete, now to make it recursive

--- workHorse.py
+++ workHorse.py
@@ -9,77 +9,106 @@ from os.path import expanduser
 
 class workHorse:
 
-    def __init__(self, filename, trace):
-        self.filename = filename
+    def __init__(self, pathToPom, trace):
+        self.pathToPom = pathToPom
         self.trace = trace
         self.m2dir = expanduser('~') + '/.m2/repository/'
-        self.parent_data = []
-        self.parent_data2 = []
+
+        self.listOfPomsAndAttributes = []
+
         if os.path.isfile(self.trace):
             os.remove(self.trace)
 
     def currentPomAttributes(self):
         try:
-            fh = open(self.filename, 'r')
+            fh = open(self.pathToPom, 'r')
         except OSError:
-            print("Oops that was not supposed to happen. Bye..." + str(OSError.errno))
+            print("Oops that was not supposed to happen. Bye...")
             sys.exit(1)
 
-        try:
-            self.wh = open(self.trace, 'x')
-        except OSError:
-            print("Oops that was not supposed to happen. Bye..." + str(OSError.errno))
-            sys.exit(1)
+        self.soup = BeautifulSoup(fh, "xml")
+        fh.close()
 
-        soup = BeautifulSoup(fh, "xml")
+        self.parent_data = []
+        self.module_data = []
+        self.current_pom_data = []
 
-        groupId_list = soup.find_all('groupId')
-        artifactId_list = soup.find_all('artifactId')
-        packaging_list = soup.find_all('packaging')
-        name_list = soup.find_all('name')
-        version_list = soup.find_all('version')
-        relativePath_list = soup.find_all('relativePath')
-        module_list = soup.find_all('modules')
+        self.checkListForParent('groupId')
+        self.checkListForParent('artifactId')
+        self.checkListForParent('packaging')
+        self.checkListForParent('name')
+        self.checkListForParent('version')
+        self.checkListForParent('relativePath')
+        self.checkListForParent('module')
 
-        self.checkListForParent(groupId_list)
-        self.checkListForParent(artifactId_list)
-        self.checkListForParent(packaging_list)
-        self.checkListForParent(name_list)
-        self.checkListForParent(version_list)
-        self.checkListForParent(relativePath_list)
-        self.checkListForParent(module_list)
+        parentDict = self.parentDataToDict(self.parent_data)
+        moduleList = self.moduleDataToDict(self.module_data)
+        currentPomDict = self.currentPomDataToDict(self.current_pom_data)
 
-        self.extract_parent_data(self.parent_data)
+        levelDict = {'current': currentPomDict,
+                     'parent': parentDict,
+                     'module': moduleList}
 
-        for i in self.parent_data2:
-            print(str(i.keys()) + '\n')
-            print(str(i.values()) + '\n')
+        self.listOfPomsAndAttributes.append(levelDict)
 
-        #self.parent_data2.
+    def checkListForParent(self, search_string):
+        the_list = self.soup.find_all(search_string)
 
-        self.wh.close()
-
-    def checkListForParent(self, the_list):
         for element in the_list:
             parent_info = element.find_parent()
             if parent_info.name == 'parent':
                 self.parent_data.append(element)
+            elif parent_info.name == 'modules':
+                self.module_data.append(element)
             else:
-                self.writeToFile(element)
+                self.current_pom_data.append(element)
 
-    def writeToFile(self, tag):
-        #self.wh.write(str(self.filename))
-        #self.wh.write('\n')
-        #self.wh.write('name:')
-        #self.wh.write(tag.name)
-        #self.wh.write('| string:')
-        #self.wh.write(tag.string)
-        #self.wh.write('| tag:')
-        self.wh.write(str(tag))
-        self.wh.write('\n\n')
-
-    def extract_parent_data(self, listOfTagsFromParent):
+    def parentDataToDict(self, listOfTagsFromParent):
         parentDict = {}
         for element in listOfTagsFromParent:
             parentDict.update({element.name: element.string})
-        self.parent_data2.append(parentDict)
+
+        if len(listOfTagsFromParent) > 0:
+            self.pathToPom = self.createNewFileName(parentDict)
+        else:
+            self.pathToPom = None
+        return parentDict
+
+    def createNewFileName(self, parentDict):
+        group = parentDict['groupId']
+        group = group.replace('.', '/')
+        directoryToPom = str(self.m2dir) + str(group) + '/' + str(parentDict['artifactId']) + '/' \
+                         + str(parentDict['version']) + '/' + str(parentDict['artifactId']) + '-' \
+                         + str(parentDict['version']) + '.pom'
+        print(directoryToPom)
+
+    def moduleDataToDict(self, listOfTagsFromParent):
+        moduleList = []
+        for element in listOfTagsFromParent:
+            moduleList.append({element.name: element.string})
+        return moduleList
+
+    def currentPomDataToDict(self, listOfTagsFromParent):
+        currentPomDict = {}
+        for element in listOfTagsFromParent:
+            currentPomDict.update({element.name: element.string})
+        return currentPomDict
+
+    def writeToFile(self, tag):
+        try:
+            wh = open(self.trace, 'x')
+        except OSError:
+            print("Oops that was not supposed to happen. Bye..." + str(OSError.errno))
+            sys.exit(1)
+
+        wh.write(str(self.pathToPom))
+        wh.write('\n')
+        wh.write('name:')
+        wh.write(tag.name)
+        wh.write('| string:')
+        wh.write(tag.string)
+        wh.write('| tag:')
+        wh.write(str(tag))
+        wh.write('\n\n')
+
+        wh.close()
